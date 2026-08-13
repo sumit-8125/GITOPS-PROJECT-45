5c6bdf0bd6cb1aac209e6c90a8c4a03eb54e607e
sumit-8125 <sumitchoudhary68104@gmail.com>
2026-08-14 01:11:13 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -2,3 +2,4 @@ variable "vpc_cidr" { type = string }
 variable "subnets" { type = list(string) }
 variable "route_table" { type = list(string) } 
 variable "project_rta" { type = list(string) }
+varibale "node_policy" { type = list(string) }
