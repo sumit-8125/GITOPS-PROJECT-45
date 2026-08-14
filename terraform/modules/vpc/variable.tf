8acd57c68d61bc34a4be90b8906cbeb7240f0265
sumit-8125 <sumitchoudhary68104@gmail.com>
2026-08-14 16:06:51 +0530

    Update variable.tf

--- terraform/modules/vpc/variable.tf
+++ terraform/modules/vpc/variable.tf
@@ -2,7 +2,7 @@ variable "vpc_cidr" {
 type = string 
 }
 
-variable "subents" {
+variable "subnets" {
  type = map(object({
  cidr_subnet = string 
 AZs = string 
@@ -20,30 +20,12 @@ nat_gateway_id = optional(string)
 
 variable "route_association" {
 type = map(object({
-subent_key = string 
+subnet_key = string 
 route_table_key = string 
 }))
 }
 
 
-variable "sg_group" {
-type = map(object({
-
-ingress = list(object({
-from_port = string 
-to_port = string 
-protocol = string 
-cidr_block = string
-}))
-
-egress = list(object({
-from_port = string 
-to_port = string 
-protocol = string 
-cidr_block = string
-}))
-} ))
-}
 
 
 
