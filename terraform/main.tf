e59e911146e9c29c3de6ea0793d0491f7845b60a
sumit-8125 <sumitchoudhary68104@gmail.com>
2026-08-14 16:07:19 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,9 @@ modules "vpc" {
  source = "./modules/vpc"
  vpc_cidr = var.vpc_cidr
  subnets = var.subents 
- route_table = var.route_table
+route_table = var.route_table
+ route_association = var.route_association
 project_rta = var.project_rta 
-sg_for_project = var.sg_for_project
 }
 
 modules "iam" {
