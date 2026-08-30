6c26b88b7e2cdf34fbbe6f054ce91b665014a93b
sumit <sumitchoudhary68104@gmail.com>
2026-08-30 13:54:13 +0530

    feat(terraform): configure s3 remote state backend gitops-project-45-bucket

--- Terraform/versions.tf
+++ Terraform/versions.tf
@@ -1,13 +1,11 @@
 terraform {
   required_version = ">= 1.6.0"
 
-  # Remote State Storage:
-  # Agar aapne S3 bucket banaya hai, toh neeche wala backend block uncomment karke apna bucket name daal dein:
-  # backend "s3" {
-  #   bucket = "YOUR_UNIQUE_S3_BUCKET_NAME"
-  #   key    = "hitman-cloudflux/prod/terraform.tfstate"
-  #   region = "ap-south-1"
-  # }
+  backend "s3" {
+    bucket = "gitops-project-45-bucket"
+    key    = "hitman-cloudflux/prod/terraform.tfstate"
+    region = "ap-south-1"
+  }
 
   required_providers {
     aws = {
